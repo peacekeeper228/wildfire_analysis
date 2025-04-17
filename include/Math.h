4531b124cb24fd817bbeb516356c3f0646e29a66
peacekeeper228 <sasa.osipov.01@yandex.ru>
2025-04-17 23:11:24 +0300

    add wind meta data

--- include/Math.h
+++ include/Math.h
@@ -16,6 +16,17 @@ struct SlopeMetaData
     }
 };
 
+struct WindMetaData
+{
+    int direction_difference_;
+    double wind_speed_;
+    bool operator==(const WindMetaData &rhs) const
+    {
+        return direction_difference_ == rhs.direction_difference_ &&
+               wind_speed_ == rhs.wind_speed_;
+    }
+};
+
 template <>
 struct std::hash<SlopeMetaData>
 {
@@ -27,32 +38,45 @@ struct std::hash<SlopeMetaData>
     }
 };
 
+
+template <>
+struct std::hash<WindMetaData>
+{
+    std::size_t operator()(const WindMetaData &key) const
+    {
+        size_t h1 = std::hash<int>()(key.direction_difference_);
+        size_t h2 = std::hash<int>()(key.wind_speed_);
+        return h1 ^ (h2 << 1);
+    }
+};
+
 class Math
 {
 private:
-    
 public:
     Math();
-    //virtual double calculateKoef(float windSpeed, double slopeAngleRad) const = 0;
-    //virtual double calculateWindKoef(const cell* c, directions InvestigatedDirection) const = 0;
-    virtual bool willSpread(const cell* c, directions InvestigatedDirection, int altitudeDifference) const = 0;
-    virtual bool willSpreadThroughOne(const cell* c, directions InvestigatedDirection, int altitudeDifference) const = 0;
+    // virtual double calculateKoef(float windSpeed, double slopeAngleRad) const = 0;
+    // virtual double calculateWindKoef(const cell* c, directions InvestigatedDirection) const = 0;
+    virtual bool willSpread(const cell *c, directions InvestigatedDirection, int altitudeDifference) const = 0;
+    virtual bool willSpreadThroughOne(const cell *c, directions InvestigatedDirection, int altitudeDifference) const = 0;
     ~Math();
 };
 
-constexpr int ignitionPercentage(){
+constexpr int ignitionPercentage()
+{
     return 50; // in formula there is smth wrong I suppose
 }
 
-constexpr int throughPercentage(){
+constexpr int throughPercentage()
+{
     return 100; // in formula there is smth wrong I suppose
 }
 
 /// @brief by now is from
-/// "Aildland fire spread modelling using cellular 
-/// automata: evolution in large-scale spatially heterogeneous environments 
+/// "Aildland fire spread modelling using cellular
+/// automata: evolution in large-scale spatially heterogeneous environments
 /// under fire suppression tactics"
-class Math1 final:  public Math
+class Math1 final : public Math
 {
 private:
     mutable std::unordered_map<SlopeMetaData, int> slope_counter_;
@@ -62,12 +86,12 @@ private:
     mutable clock_t slope_timer;
     mutable int slope_counter;
     double calculateKoef(float windSpeed, double slopeAngleRad) const;
-    double calculateWindKoef(const cell* c, directions InvestigatedDirection) const;
+    double calculateWindKoef(const cell *c, directions InvestigatedDirection) const;
     double calculateGroundSlopeKoef(directions InvestigatedDirection, int altitudeDifference) const;
+
 public:
-    bool willSpread(const cell* c, directions InvestigatedDirection, int altitudeDifference) const override;
-    bool willSpreadThroughOne(const cell* c, directions InvestigatedDirection, int altitudeDifference) const override;
+    bool willSpread(const cell *c, directions InvestigatedDirection, int altitudeDifference) const override;
+    bool willSpreadThroughOne(const cell *c, directions InvestigatedDirection, int altitudeDifference) const override;
     Math1();
     ~Math1();
 };
-
